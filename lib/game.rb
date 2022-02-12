67e24e7767e89bfab41e1f6d534f601478d63af7
Susan Boyd <sueboyd922@gmail.com>
2022-02-12 14:07:14 -0500

    feat: condense winning scenarios into game class

--- lib/game.rb
+++ lib/game.rb
@@ -41,20 +41,72 @@ class Game
       @player.get_input
       @pieces_played += 1
       break if @player.quit == true
-      break if @player.winner? == true
+      break if winner?(@player) == true
       @computer.random_letter
       @pieces_played += 1
-      break if @computer.winner? == true
+      break if winner?(@computer) == true
     end
     if @pieces_played == 42
       puts "The board is full! It's a draw"
-    elsif @player.winner?
+    elsif winner?(@player)
       puts "You beat a dumb computer...congrats"
-    elsif @computer.winner?
+    elsif winner?(@computer)
       puts "The computer beat you...sad"
     end
   end
 
+  def winner?(whose_turn)
+    if whose_turn.instance_of?(Player)
+      letters = "X X X X"
+    else
+      letters = "O O O O"
+    end
+
+    if check_for_diagonal(letters) == false
+      return true
+    elsif check_for_horizontal(letters) == false
+      return true
+    elsif check_for_vertical(letters) == false
+      return true
+    end
+  end
+
+  def check_for_horizontal(letters)
+    connect = @board.lines.select do |column|
+          column.join.include?(letters)
+        end
+    connect.empty?
+  end
+
+  def check_for_vertical(letters)
+    column_check = @board.lines.transpose
+    connect = column_check.select do |column|
+            column.join.include?(letters)
+          end
+    connect.empty?
+  end
+
+  def check_for_diagonal(letters)
+    diagonal_lines = [
+      [@board.lines[2][0], @board.lines[3][1], @board.lines[4][2], @board.lines[5][3]],
+      [@board.lines[1][0], @board.lines[2][1], @board.lines[3][2], @board.lines[4][3], @board.lines[5][4]],
+      [@board.lines[0][0], @board.lines[1][1], @board.lines[2][2], @board.lines[3][3], @board.lines[4][4], @board.lines[5][5]],
+      [@board.lines[0][1], @board.lines[1][2], @board.lines[2][3], @board.lines[3][4], @board.lines[4][5], @board.lines[5][6]],
+      [@board.lines[0][2], @board.lines[1][3], @board.lines[2][4], @board.lines[3][5], @board.lines[4][6]],
+      [@board.lines[0][3], @board.lines[1][4], @board.lines[2][5], @board.lines[3][6]],
+      [@board.lines[3][0], @board.lines[2][1], @board.lines[1][2], @board.lines[0][3]],
+      [@board.lines[4][0], @board.lines[3][1], @board.lines[2][2], @board.lines[1][3], @board.lines[0][4]],
+      [@board.lines[5][0], @board.lines[4][1], @board.lines[3][2], @board.lines[2][3], @board.lines[1][4], @board.lines[0][5]],
+      [@board.lines[5][1], @board.lines[4][2], @board.lines[3][3], @board.lines[2][4], @board.lines[1][5], @board.lines[0][6]],
+      [@board.lines[5][2], @board.lines[4][3], @board.lines[3][4], @board.lines[2][5], @board.lines[1][6]],
+      [@board.lines[5][3], @board.lines[4][4], @board.lines[3][5], @board.lines[2][6]]
+      ]
+
+      connect = diagonal_lines.select do |column|
+          column.join.include?(letters)
+          end
+      connect.empty?
+    end
 end
 
 game = Game.new
